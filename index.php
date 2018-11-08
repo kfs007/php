89a41b8cf0a71741032ff41f9fa95431c07822b8
kfs007 <44784940+kfs007@users.noreply.github.com>
2018-11-08 15:36:44 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php 
 
-<<<<<<< HEAD
 	echo"Olá mundo! 456";
 
-	echo "Olá Udemy";
-=======
-	echo"Olá mundo123!";
-
-	echo "Olá Hcode";
->>>>>>> f6de7ba47695d068891c620752646e4fab6bed7f
+	echo "Olá Udemy e Hcode";
 
 ?>
